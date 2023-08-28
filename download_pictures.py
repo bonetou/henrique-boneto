e2e5302f1de0a9d6c0a09d4e683d99a4d2cce24a
Henrique Boneto <h.boneto@outlook.com>
2023-08-28 05:08:34 -0300

    rename tasks

--- download_pictures.py
+++
@@ -1,9 +0,0 @@
-from src.download_pictures import PictureDownloader
-from robocorp.tasks import task
-from robocorp import workitems
-
-
-@task
-def download_news_pictures():
-    payload = workitems.inputs.current.payload
-    PictureDownloader.download(payload["news"])
--- pictures_and_report_task.py
+++ pictures_and_report_task.py
@@ -1,9 +1,11 @@
+from excel_report import ExcelReport
+from src.download_pictures import PictureDownloader
 from robocorp.tasks import task
-from src.excel_report import ExcelReport
 from robocorp import workitems
 
 
 @task
-def generate_excel_report():
+def download_pictures_and_generate_report():
     payload = workitems.inputs.current.payload
+    PictureDownloader.download(payload["news"])
     ExcelReport.generate(payload["news"], payload["searchPhrase"])
